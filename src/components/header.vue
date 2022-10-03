32d4445757255dbe2b15e6c163db56e93eba7fcc
UYulong <1274583083@qq.com>
2022-10-04 00:08:01 +0800

    config husky lint

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,21 +2,8 @@
   <div></div>
 </template>
 
-<script
-
-lang="ts"
-
-
-setup
-
-
-name="test"
-
-
->
-const msg:
-
-string = "          hello"
+<script lang="ts" setup name="test">
+const msg: string = "          hello"
 </script>
 
 <style lang="scss" scoped></style>

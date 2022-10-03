6e3b12d7edafd74db892533b4abb9a67c292e232
UYulong <1274583083@qq.com>
2022-10-03 23:54:52 +0800

    add husky

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,11 @@
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+
+
+
+
+      <img                  src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
